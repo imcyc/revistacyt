f74d9428fb4330aae868373f02dd4917142b6502
Axel <axosar@gmail.com>
2019-11-11 11:07:30 -0600

    Updates

---
+++ precache-manifest.b0f04dda1a93ea294a7bcf8c2d9d1f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf99cc880b057b97a3ffa426ac74c3c",
+    "url": "/revistacyt/index.html"
+  },
+  {
+    "revision": "a8890b47e16b5f3204b9",
+    "url": "/revistacyt/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9670da1268eee85f684c",
+    "url": "/revistacyt/static/js/2.d8d6a889.chunk.js"
+  },
+  {
+    "revision": "a8890b47e16b5f3204b9",
+    "url": "/revistacyt/static/js/main.11fddd91.chunk.js"
+  },
+  {
+    "revision": "3e6a92a0c046763dc1e1",
+    "url": "/revistacyt/static/js/runtime-main.35a20991.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/revistacyt/static/media/logo.25bf045c.svg"
+  }
+]);
