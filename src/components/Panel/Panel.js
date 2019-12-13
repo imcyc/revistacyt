b8d6c1b4b6c8c42970333636b7cddf6606564bce
Axel <axosar@gmail.com>
2019-12-12 18:30:38 -0600

    Noticias Ticker

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -52,8 +52,8 @@ function Panel(props) {
               <div className="contenido">
                 <Row style={{height: '100%', overflow: 'hidden', padding: '10px'}}>
                 {data.map(item => (
-                  <Col xs={12} sm={4} md={4} lg={3}>
-                    <CardArticulo key={item.author} title={item.title} description={item.description} urlToImage={item.urlToImage} />
+                  <Col xs={12} sm={4} md={4} lg={3} key={item.author}>
+                    <CardArticulo title={item.title} description={item.description} urlToImage={item.urlToImage} />
                   </Col>
                 ))}
                 </Row>
